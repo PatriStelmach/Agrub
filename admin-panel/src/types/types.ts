773adc479871d5c7010f4a8da308ce677635389b
stelmach.p <patrzy108@gmail.com>
2026-04-17 14:55:46 +0200

    deleted lastModifiedBy

--- admin-panel/src/types/types.ts
+++ admin-panel/src/types/types.ts
@@ -37,33 +37,19 @@ export interface Plugin
   id: number,
   name: string,
   creator: string,
-  lastModifiedBy: string,
   language: "python" | "bash" | "PowerShell",
   description?: string,
   code: string,
   weight: number,
   createdAt?: Date,
   tags: string[]
-  runningIntervals?: number,
+  cron?: string
   addedAt?: Date,
   updatedAt?: Date,
   on?: boolean,
   type?: "log" | "alert"
 }
-//
-// export interface Plugin
-// {
-//   id: number,
-//   name: string,
-//   lastModifiedBy: user(pewnie id, więc może być number)
-//   description?: string,
-//   code?: string,
-//   tags?: string[]
-//   runningIntervals?: number (w sekundach)
-//   updatedAt: (api musi zmienić na now())
-//   on?: boolean,
-//   type?: "log" | "alert"
-// }
+
 
 export interface System
 {
