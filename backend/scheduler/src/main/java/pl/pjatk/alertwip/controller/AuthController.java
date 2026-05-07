ddfb33dd7b54c9cae5f75e0b869a63fe15fae9d3
mati <matandrzejak@outlook.com>
2026-05-07 18:53:06 +0200

    Dodanie refresh token

--- backend/scheduler/src/main/java/pl/pjatk/alertwip/controller/AuthController.java
+++ backend/scheduler/src/main/java/pl/pjatk/alertwip/controller/AuthController.java
@@ -1,10 +1,16 @@
 package pl.pjatk.alertwip.controller;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import pl.pjatk.alertwip.dto.AuthenticationRequest;
-import pl.pjatk.alertwip.dto.AuthenticationResponse;
+import pl.pjatk.alertwip.dto.AuthenticationRequestDTO;
+import pl.pjatk.alertwip.dto.AuthenticationResponseDTO;
 import pl.pjatk.alertwip.service.AuthenticationService;
+
+import java.util.Arrays;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -17,9 +23,32 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<AuthenticationResponse> authenticate(
-            @RequestBody AuthenticationRequest request
+    public ResponseEntity<AuthenticationResponseDTO> login(
+            @RequestBody AuthenticationRequestDTO request,
+            HttpServletResponse response
     ) {
-        return ResponseEntity.ok(authenticationService.authenticate(request));
+        Map<String, String> tokens = authenticationService.authenticate(request);
+
+        Cookie refreshCookie = new Cookie("refresh_token", tokens.get("refresh_token"));
+        refreshCookie.setHttpOnly(true);
+        refreshCookie.setSecure(false); // w prod na true (wymaga https)
+        refreshCookie.setPath("/");
+        refreshCookie.setMaxAge(24 * 60 * 60); // 1 dzień
+
+        response.addCookie(refreshCookie);
+
+        return ResponseEntity.ok(new AuthenticationResponseDTO(tokens.get("access_token")));
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<AuthenticationResponseDTO> refresh(HttpServletRequest request) {
+        String refreshToken = Arrays.stream(request.getCookies())
+                .filter(c -> "refresh_token".equals(c.getName()))
+                .findFirst()
+                .map(Cookie::getValue)
+                .orElseThrow(() -> new RuntimeException("No refresh token found"));
+
+        String newAccessToken = authenticationService.refreshAccessToken(refreshToken);
+        return ResponseEntity.ok(new AuthenticationResponseDTO(newAccessToken));
     }
 }
