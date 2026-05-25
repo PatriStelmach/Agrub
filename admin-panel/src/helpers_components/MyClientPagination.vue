2474e79f9b6f07b9acdfed22364ea427a62f11bd
just_pa3 <115350109+PatriStelmach@users.noreply.github.com>
2026-05-25 10:01:23 +0200

    font pagination

--- admin-panel/src/helpers_components/MyClientPagination.vue
+++ admin-panel/src/helpers_components/MyClientPagination.vue
@@ -99,7 +99,7 @@ watchEffect(() => {
         </DropdownMenuContent>
       </DropdownMenu>
 
-      <span>Results: <span class="font-extrabold">{{ total}}</span></span>
+      <span class="text-md" >Results: <span class="font-extrabold text-md">{{ total}}</span></span>
     </div>
   </Pagination>
 </template>
