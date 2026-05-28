4ed37a1f3c1b1a70e6498d59bda23ac6a95e6f55
Patri Stelmach <patrzy108@gmail.com>
2026-05-28 19:22:53 +0200

    proszę ogarnąć te wykresy, chyba że chcemy pętlę for na 200k rekordów robić hehe

--- backend/scheduler/src/main/java/pl/pjatk/alertwip/controller/AnalyticsController.java
+++ backend/scheduler/src/main/java/pl/pjatk/alertwip/controller/AnalyticsController.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/analytics")
+@RequestMapping("/api/analitycs")
 public class AnalyticsController {
 
     private final AnalyticsService analyticsService;
