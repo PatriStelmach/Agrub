2e9d8688da911fddd6c209412366a8551e4228bb
Patri Stelmach <patrzy108@gmail.com>
2026-07-09 00:32:23 +0200

    description padding

--- admin-panel/src/pages/login/LoginPage.vue
+++ admin-panel/src/pages/login/LoginPage.vue
@@ -69,7 +69,7 @@ const email = ref("")
     <Tabs :class="{'blur-xl' : showWrongPassword || showResetPassword}" class="left-1/2 absolute -translate-x-1/2 top-1/2 translate-y-1/2 duration-300" default-value="ALERT">
       <TabsList class="mx-auto *:cursor-pointer">
         <TabsTrigger :disabled="showWrongPassword" value="ALERT">
-          Alert credentials
+          Agrub credentials
         </TabsTrigger>
         <TabsTrigger :disabled="showWrongPassword" value="AD">
           Active Directory credentials
