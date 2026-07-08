71d10a911856776593fcfb5866a720d91367ea22
Patri Stelmach <patrzy108@gmail.com>
2026-07-09 00:30:23 +0200

    description padding

--- admin-panel/src/pages/plugins/PluginDetailsDialog.vue
+++ admin-panel/src/pages/plugins/PluginDetailsDialog.vue
@@ -62,8 +62,8 @@ const emits = defineEmits<{
 
         </DialogHeader>
         <div class="grid gap-4">
-          <div class="grid grid-cols-3">
-            <div class="border-b-2 border-accent  col-span-1 gap-3">
+          <div :class="{'border-b-2 border-accent' : !editable}" class="grid grid-cols-3">
+            <div class="col-span-1 gap-3">
               <Label class="text-lg" for="my-plugin-description">Description</Label>
               <h2 class="text-sm text-comment">Description of this plugin</h2>
               <Transition name="fade" mode="out-in">
@@ -77,7 +77,7 @@ const emits = defineEmits<{
                 <h1 class=" pt-2 pr-8 w-fit" id="show_plugin_description" v-else>{{ description}}</h1>
               </Transition>
             </div>
-            <div class="border-b-2 border-accent  col-span-2 gap-3">
+            <div class="col-span-2 gap-3">
               <Label class="text-lg" for="my_plugin_arguments">Arguments</Label>
               <h2 class="text-sm text-comment">Set execute arguments for script
                 Use space between arguments: "arg1 arg2 arg3"
